2ad87b8111373141c7f196a325a949ec0ea9d923
mattea <mattea@ccs.neu.edu>
2014-12-29 16:33:38 -0500

    Test skeleton

---
+++ NUIR-TS14/src/test/java/edu/neu/ccs/nuir/tempsum/TemporalSummarizationTest.java
@@ -0,0 +1,19 @@
+package edu.neu.ccs.nuir.tempsum;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TemporalSummarizationTest {
+
+	@Test
+	public void testTemporalSummarization() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+}
