d6de986387e8ed084fa8975677a0df06d4fac76f
mattea <mattea@ccs.neu.edu>
2014-12-30 14:04:05 -0500

    adding ResultSet

---
+++ NUIR-TS14/src/main/java/edu/neu/ccs/nuir/tempsum/ResultSet.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package edu.neu.ccs.nuir.tempsum;
+
+import java.util.ArrayList;
+
+/**
+ * @author mattea
+ *
+ */
+public class ResultSet {
+	public ArrayList<Sentence> sentences;
+	public double threshold;
+}
